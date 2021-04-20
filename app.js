5633793b3f59f240488ed7f50cc4b4c88cfae383
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-04-19 17:12:03 -0700

    added some posts and connected mongoDB

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv/config");
+
+// Import Routes
+const postsRoute = require("./routes/posts");
 
 // Middleware
-// app.use("/posts", () => {
-//   console.log("This is a middleware running");
-// });
+app.use("/posts", postsRoute);
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("We are on home");
 });
 
-app.get("/posts", (req, res) => {
-  res.send("We are on posts");
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log("Connected to DB!!!");
 });
 
 app.listen(3000);
