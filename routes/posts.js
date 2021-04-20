9571931d2774817f8604f0c6d7883cae7d281d56
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-04-20 12:45:27 -0700

    working with mongoDB

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("We are on posts");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  post
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
